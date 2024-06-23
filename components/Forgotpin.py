cdf8b1376ba400f7e455a9b47e48487b089c9f7d
dselokela023 <dselokela023@student.wethinkco.de>
2024-06-23 15:08:46 +0200

    forgot pin function to set a new pin

---
+++ components/Forgotpin.py
@@ -0,0 +1,16 @@
+from Signin import*
+
+def forgot():
+    while True:
+        recoverypin = str(input("Create a new 6 digits pin: "))
+        if len(recoverypin) != 6:
+            print("The pin has to be 6 digits")
+        else:
+            confirmpin = str(input("Confirm your 6 digit pin: "))
+            if recoverypin != confirmpin :
+                print("New pin does not math")
+            else:
+                pin = confirmpin
+                return confirmpin
+
+print(forgot())
